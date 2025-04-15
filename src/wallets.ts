65220c92dcb32b2283ad3268d1839345eabef971
oleh <olehmisar@gmail.com>
2025-04-15 16:24:04 +0200

    format

--- src/wallets.ts
+++ src/wallets.ts
@@ -1,25 +1,25 @@
 import { RemoteArtifactStrategy } from "./artifacts.js";
 import type { AztecWalletSdk } from "./base.js";
 import {
-	type ObsidionBridgeConnectorOptions,
-	ObsidionBridgeConnector,
+  type ObsidionBridgeConnectorOptions,
+  ObsidionBridgeConnector,
 } from "./obsidion.js";
 
 type PartialObsidionPopupConnectorOptions = Partial<
-	Pick<ObsidionBridgeConnectorOptions, "walletUrl" | "artifactStrategy">
+  Pick<ObsidionBridgeConnectorOptions, "walletUrl" | "artifactStrategy">
 >;
 
 export function obsidion(params: PartialObsidionPopupConnectorOptions) {
-	return (sdk: AztecWalletSdk) =>
-		new ObsidionBridgeConnector({
-			...params,
-			fallbackOpenPopup: sdk.fallbackOpenPopup,
-			walletUrl: params.walletUrl ?? "https://app.obsidion.xyz",
-			artifactStrategy:
-				params.artifactStrategy ?? RemoteArtifactStrategy.getDefaultSingleton(),
+  return (sdk: AztecWalletSdk) =>
+    new ObsidionBridgeConnector({
+      ...params,
+      fallbackOpenPopup: sdk.fallbackOpenPopup,
+      walletUrl: params.walletUrl ?? "https://app.obsidion.xyz",
+      artifactStrategy:
+        params.artifactStrategy ?? RemoteArtifactStrategy.getDefaultSingleton(),
 
-			uuid: "obsidion",
-			name: "Obsidion",
-			icon: "https://pbs.twimg.com/profile_images/1849068253685116928/MzTzv03r_400x400.jpg",
-		});
+      uuid: "obsidion",
+      name: "Obsidion",
+      icon: "https://pbs.twimg.com/profile_images/1849068253685116928/MzTzv03r_400x400.jpg",
+    });
 }
