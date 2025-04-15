4c9625a9c98b6b5609de22b9a695f0ccd0060517
Porco <porcorossoj89@gmail.com>
2025-04-15 15:13:21 +0400

    obsidion/bridge

--- src/wallets.ts
+++ src/wallets.ts
@@ -1,27 +1,25 @@
 import { RemoteArtifactStrategy } from "./artifacts.js";
 import type { AztecWalletSdk } from "./base.js";
 import {
-  ReownPopupConnector,
-  type ReownPopupConnectorOptions,
-} from "./reownPopup.js";
+	type ObsidionBridgeConnectorOptions,
+	ObsidionBridgeConnector,
+} from "./obsidion.js";
 
-type PartialReownPopupConnectorOptions = Pick<
-  ReownPopupConnectorOptions,
-  "projectId" | "metadata"
-> &
-  Partial<Pick<ReownPopupConnectorOptions, "walletUrl" | "artifactStrategy">>;
+type PartialObsidionPopupConnectorOptions = Partial<
+	Pick<ObsidionBridgeConnectorOptions, "walletUrl" | "artifactStrategy">
+>;
 
-export function obsidion(params: PartialReownPopupConnectorOptions) {
-  return (sdk: AztecWalletSdk) =>
-    new ReownPopupConnector({
-      ...params,
-      fallbackOpenPopup: sdk.fallbackOpenPopup,
-      walletUrl: params.walletUrl ?? "https://app.obsidion.xyz",
-      artifactStrategy:
-        params.artifactStrategy ?? RemoteArtifactStrategy.getDefaultSingleton(),
+export function obsidion(params: PartialObsidionPopupConnectorOptions) {
+	return (sdk: AztecWalletSdk) =>
+		new ObsidionBridgeConnector({
+			...params,
+			fallbackOpenPopup: sdk.fallbackOpenPopup,
+			walletUrl: params.walletUrl ?? "https://app.obsidion.xyz",
+			artifactStrategy:
+				params.artifactStrategy ?? RemoteArtifactStrategy.getDefaultSingleton(),
 
-      uuid: "obsidion",
-      name: "Obsidion",
-      icon: "https://pbs.twimg.com/profile_images/1849068253685116928/MzTzv03r_400x400.jpg",
-    });
+			uuid: "obsidion",
+			name: "Obsidion",
+			icon: "https://pbs.twimg.com/profile_images/1849068253685116928/MzTzv03r_400x400.jpg",
+		});
 }
